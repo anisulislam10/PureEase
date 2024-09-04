65e7f1a69de267251e3363945f818ba993ff5fb7
Anisul Islam <anis.inbox10@gmail.com>
2024-09-04 14:43:01 -0700

    added images from web link

--- frontend/src/components/reuseable_images/FeaturedProductCard.js
+++ frontend/src/components/reuseable_images/FeaturedProductCard.js
@@ -1,7 +1,7 @@
 const products = [
     {
       id: 1,
-      imageSrc: '/src/assets/Featured-Products/1.jpg',
+      imageSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjDGWkmWsT6kzz7Hd_HyRyI0qd2h9BSs3nHQ&s',
       description: 'Pack of 2 - Eco-Friendly Bamboo Tissue Set',
       price: 999.99,
       altText: 'Eco-Friendly Bamboo Tissue Set',
@@ -15,7 +15,7 @@ const products = [
     },
     {
       id: 3,
-      imageSrc: '/src/assets/Featured-Products/3.webp',
+      imageSrc: 'https://pure-ease.vercel.app/assets/3-C3cdTzth.jpg',
       description: 'Paper Napkin Use For Home, Office',
       price: 300.99,
       altText: 'Paper Napkin Use For Home, Office',
--- frontend/src/components/reuseable_images/products.js
+++ frontend/src/components/reuseable_images/products.js
@@ -1,7 +1,7 @@
 const products = [
     {
       id: 1,
-      imageSrc: '/src/assets/6.png',
+      imageSrc: 'https://rukminim2.flixcart.com/image/850/1000/kd3f3bk0/wipe/m/6/7/soft-premium-face-tissue-paper-box-white-2-ply-100-pulls-pack-of-original-imafu2tnwewzqyje.jpeg?q=90&crop=false',
       description: 'tissue paper',
       price: 1000.99,
       rating: 4.5,
@@ -9,7 +9,7 @@ const products = [
     },
     {
       id: 2,
-      imageSrc: '/src/assets/1.png',
+      imageSrc: 'https://hotpack.com.sa/cdn/shop/products/SNCtissue200x2PLY-single-open_1_71f0879e-fa10-4d60-84d3-6f075decae6f_1.jpg?v=1723015549',
       description: 'tissue paper',
       price: 1000.99,
       rating: 4.5,
@@ -17,7 +17,7 @@ const products = [
     },
     {
       id: 3,
-      imageSrc: '/src/assets/2.png',
+      imageSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTw0dhxQO9vL_rws_2p1orOrogYGju4z1MF4SVLcZHYvR7qgFRwSKMkYD3C9-93lgCbF0&usqp=CAU',
       description: 'tissue paper',
       price: 1000.99,
       rating: 4.5,
@@ -25,7 +25,7 @@ const products = [
     },
     {
       id: 4,
-      imageSrc: '/src/assets/4.webp',
+      imageSrc: 'https://www.bakerross.co.uk/media/catalog/product/r/e/red-pink-purple-tissue-paper-value-pack-aw118j.jpg',
       description: 'tissue paper',
       price: 1000.99,
       rating: 4.5,
@@ -33,7 +33,7 @@ const products = [
     },
     {
       id: 5,
-      imageSrc: '/src/assets/3.jpg',
+      imageSrc: 'https://stationers.pk/cdn/shop/products/rosepetalluxury.jpg?v=1673354482&width=600',
       description: 'tissue paper',
       price: 1000.99,
       rating: 4.5,
@@ -41,14 +41,14 @@ const products = [
     },
     {
       id: 6,
-      imageSrc: '/src/assets/8.webp',
+      imageSrc: 'https://pure-ease.vercel.app/assets/3-C3cdTzth.jpg',
       price: 1000.99,
       rating: 4.5,
       altText: 'tissue paper ',
     },
     {
       id: 7,
-      imageSrc: '/src/assets/7.webp',
+      imageSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjDGWkmWsT6kzz7Hd_HyRyI0qd2h9BSs3nHQ&s',
       description: 'tissue paper',
       price: 1000.99,
       rating: 4.5,
@@ -56,7 +56,7 @@ const products = [
     },
     {
       id: 8,
-      imageSrc: '/src/assets/6.png',
+      imageSrc: 'https://m.media-amazon.com/images/I/71UxyvXl-ML._AC_SL1200_.jpg',
       description: 'tissue paper',
       price: 1000.99,
       rating: 4.5,
@@ -64,7 +64,7 @@ const products = [
     },
     {
       id: 9,
-      imageSrc: '/src/assets/1.png',
+      imageSrc: 'https://5.imimg.com/data5/SELLER/Default/2021/7/VR/US/TB/8147004/tissue-paper.jpg',
       description: 'tissue paper',
       price: 1000.99,
       rating: 4.5,
@@ -72,7 +72,7 @@ const products = [
     },
     {
       id: 10,
-      imageSrc: '/src/assets/5.png',
+      imageSrc: 'https://thecutprice.com/cdn/shop/files/2_2e47ebaf-3847-4e65-9c3d-eee167505674.jpg?v=1721282786&width=1445',
       description: 'tissue paper',
       price: 1000.99,
       rating: 4.5,
